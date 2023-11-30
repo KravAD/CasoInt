5ce7c355dd93e3fcdb4e6eb3c860401c163c57a0
KravAD <dkrav@myuax.com>
2023-11-30 11:50:57 +0100

    commit 6
    Saco estructura ConsoleBox y renombro la primera estructura de console a ColorConsole.

--- Scripts.cpp
+++ Scripts.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-struct console {
+struct ColorConsole {
     static constexpr auto fb_blue = "\033[34m";
     static constexpr auto fb_white = "\033[47m";
+};
+
+struct ConsoleBox {
+    
 };
